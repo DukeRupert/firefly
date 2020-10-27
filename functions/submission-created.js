3139b91d324d60b2e0fc276f1557775a7caa0cc6
DukeRupert <loganwilliamsse@gmail.com>
2020-10-26 17:05:31 -0700

    Edited form submit function for testing.

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,10 +1,13 @@
 // Require:
 var postmark = require("postmark");
+console.log("Requirements met")
 
 // Instantiate client using Netlify env
 const token = process.env.POSTMARK;
+console.log(token);
 
 var client = new postmark.Client(token);
+console.log("Client instantiated")
 
 // Send an email:
 exports.handler = async (event, context) => {
@@ -12,7 +15,9 @@ exports.handler = async (event, context) => {
 
 // Grab form submission payload
   let payload = JSON.parse(event.body).payload;
-  let myJSON = JSON.stringify(payload.human_fields);
+  console.log(payload)
+  let myJSON = JSON.stringify(payload);
+  console.log(myJSON)
 
   try {
     client.sendEmail({
