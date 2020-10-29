dc8b3c8843c9db314fc58bb00a850cfce6acf30b
DukeRupert <loganwilliamsse@gmail.com>
2020-10-29 10:29:50 -0700

    Testing another submit function.

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,36 +1,45 @@
 // Require:
 var postmark = require("postmark");
 
-// Instantiate client using Netlify env
+// Declare env variables
 const token = process.env.POSTMARK;
+const from = process.env.FROM_ADDRESS;
+const to = process.env.TO_ADDRESS;
 
 var client = new postmark.Client(token);
 
 // Send an email:
 exports.handler = async (event, context) => {
 
+  console.log('submission created error testing');
+  let date = new Date();
 
 // Grab form submission payload
+  let body = "Form submited at ${date}"
+  ;
   let payload = JSON.parse(event.body).payload;
-  
-  let name = JSON.stringify(payload.human_fields.name);
-  let email = JSON.stringify(payload.human_fields.email);
-  let message = JSON.stringify(payload.human_fields.message);
-  let timeline = JSON.stringify(payload.human_fields.timeline);
-  let budget = JSON.stringify(payload.human_fields.budget);
 
+  for (let key in payload.data) {
+    body += '${key}:    ${payload.data[key]},    '
+  }
 
+  // error testing
+  console.log(body)
+
+  // Send the email
   try {
     client.sendEmail({
-  "From": process.env.FROM_ADDRESS,
-  "To": process.env.TO_ADDRESS,
-  "Subject": "Test",
-  "TextBody": "Hello, world!"
+  "From": from,
+  "To": to,
+  "Subject": "Form Submission",
+  "TextBody": body,
 });
     return {
       statusCode: 200,
+      body: 'Email sent successfully'
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
+  console.log("Function complete.")
 }
