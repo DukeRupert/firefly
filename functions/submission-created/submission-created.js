65a084a5cdd7c7816759030726ef7aa082859d04
DukeRupert <loganwilliamsse@gmail.com>
2020-10-31 03:21:52 -0700

    Testing function

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,19 +1,45 @@
 const postmark = require('postmark');
-const client = new postmark.ServerClient(process.env.POSTMARK)
 
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
-  try {
-    client.sendEmail({
+const sendThankYouEmail = async ({ email }) => {
+  return new Promise((resolve, reject) => {
+    console.log('Sending the email');
+    const client = new postmark.ServerClient(process.env.POSTMARK);
+    console.log('Client instantiated')
+
+    const mailData = {
       "From": process.env.FROM_ADDRESS,
       "To": process.env.TO_ADDRESS,
-      "Subject": "Test",
+      "Subject": "Test Netlify Dev",
       "TextBody": "Hello from Postmark!"
+    };
+    console.log(JSON.stringify(mailData));
+
+    client.sendEmail(mailData, err => {
+      if (err) return reject(err);
+
+      resolve();
+    });
   });
+};
+
+exports.handler = async event => {
+  try {
+    const data = JSON.parse(event.body);
+    let reply;
+    
+    await sendThankYouEmail(data)
+
     return {
       statusCode: 200,
-    }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+      body: JSON.stringify({
+        message: "Email sent from production successfully"
+      })
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: e.message
+    };
   }
-}
+};
